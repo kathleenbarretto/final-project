eb279ecf5cf20a1b65c7f22daa01e057ca409b10
Kathleen Barretto <kssbarretto@gmail.com>
2014-09-01 20:33:19 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+final-project
+=============
+
+My final project
+this kdsfjkdjfd 
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+	// Typed text
+  	$(function(){
+      	$('.typed-text').typed({
+		strings: ["Hello, ^600 my name is Kathleen."],
+		typeSpeed: 60,
+		// To add a second sentence, activate the two lines below.
+		// backDelay: 4000,
+		// loop: true,
+      	});
+  	});
+
+  	// Parallax scrolling
+  	// $function(){
+  	// 	$('#about').stellar();
+  	// }
+
+});
+
+	// $(window).stellar();
+	// $('#about').stellar;	
