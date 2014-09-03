9d148d6b4fbda75f826c90a26eb09400026c07a0
Kathleen Barretto <kssbarretto@gmail.com>
2014-09-02 22:34:16 -0700

    Parallax for first section in place

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 	
 	// Typed text
-  	$(function(){
-      	$('.typed-text').typed({
+  	$('.typed-text').typed({
 		strings: ["Hello, ^600 my name is Kathleen."],
 		typeSpeed: 60,
 		// To add a second sentence, activate the two lines below.
 		// backDelay: 4000,
 		// loop: true,
-      	});
   	});
 
   	// Parallax scrolling
-  	// $function(){
-  	// 	$('#about').stellar();
-  	// }
+  	$.stellar({
+  		verticalScrolling: true,
+  		scrollProperty: 'scroll',
+  		positionProperty: 'position',
+  	});
 
 });
 
