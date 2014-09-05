e073c396788ca61f20a466319b79fdd25c9afb9e
Kathleen Barretto <kssbarretto@gmail.com>
2014-09-04 23:04:21 -0700

    new design

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,58 @@ $(document).ready(function(){
 		// loop: true,
   	});
 
-  	// Parallax scrolling
-  	$.stellar({
-  		verticalScrolling: true,
-  		scrollProperty: 'scroll',
-  		positionProperty: 'position',
+  	// slow scrolling to section
+	$('a[href*=#]').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+	    && location.hostname == this.hostname) {
+	      var $target = $(this.hash);
+	      $target = $target.length && $target
+	      || $('[name=' + this.hash.slice(1) +']');
+	      if ($target.length) {
+	        var targetOffset = $target.offset().top;
+	        $('html,body')
+	        .animate({scrollTop: targetOffset}, 1000);
+	       return false;
+	      }
+	    }
+	});
+
+	//fully responsive cover section
+	$("#cover-section").backstretch("/images/blurry.jpg");
+	function setHeight() {
+    	windowHeight = $(window).innerHeight();
+   		$('#cover-section').css('min-height', windowHeight);
+  		};
+  	
+  	setHeight();
+  
+	$(window).resize(function() {
+    	setHeight();
   	});
 
-});
+	$('#header-wrapper').sticky({topSpacing:0});
 
-	// $(window).stellar();
-	// $('#about').stellar;	
+
+
+
+
+	// Dynamic header based on scrolling
+    // var header = $('.transparent-header-wrapper');
+    // $(window).scroll(function() {
+    //     var scroll = $(window).scrollTop();
+
+    //     if (scroll >= 315) {
+    //         header.removeClass('transparent-header-wrapper').addClass('white-header-wrapper');
+    //     } else {
+    //         header.removeClass('white-header-wrapper').addClass('transparent-header-wrapper');
+    //     }
+    // });
+
+	// Parallax scrolling
+  	// $.stellar({
+  	// 	verticalScrolling: true,
+  	// 	scrollProperty: 'position',
+  	// 	positionProperty: 'position',
+  	// });
+
+});
